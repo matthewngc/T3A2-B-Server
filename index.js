7f550c931cb2ede5e0361ab9470b5b2a6995c950
matthewngc <13530@coderacademy.edu.au>
2023-02-02 18:06:49 +1100

    Added basic CRUD routes for listings

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express"
-import { ListingModel } from './db.js'
-
+import listingRoutes from './routes/listing_routes.js'
 
 const app = express()
 const port = 4002
@@ -9,22 +8,6 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.send({ info: "Steve's Jobs" }))
 
-app.get('/jobs', async (req, res) => res.send(await ListingModel.find()))
-
-app.get('/jobs/:id', (req, res) => {
-    const listing = listings[req.params.id]
-    if (listing) {
-        res.send(listing)
-    } else {
-        res.status(404).send({ error: 'Job listing not found' })
-    }
-})
-
-app.post('/jobs', (req, res) => {
-    const { title, description, company, education, experience } = req.body
-    const newJobListing = { title, description, company, education, experience }
-    listings.push(newJobListing)
-    res.status(201).send(newJobListing)
-})
+app.use('/jobs', listingRoutes)
 
 app.listen(port, () => console.log(`App running at http://localhost:${port}/`))
---
+++ routes/listing_routes.js
@@ -0,0 +1,73 @@
+import express from 'express'
+import { ListingModel } from '../db.js'
+
+const router = express.Router()
+
+// CREATE: Post job listing
+router.post('/', async (req, res) => {
+    try {
+        const { title, description, company, education, experience } = req.body
+        const newJobListing = { title, description, company, education, experience }
+        const insertedListing = await ListingModel.create(newJobListing)
+        res.status(201).send(await insertedListing.populate())
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+})
+
+// READ: Get all job listings
+router.get('/', async (req, res) => res.send(await ListingModel.find().populate()))
+
+// READ: Get one job listing by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const listing = await ListingModel.findById(req.params.id).populate()
+        if (listing) {
+            res.send(listing)
+        } else {
+            res.status(404).send({ error: 'Job listing not found!' })
+        }
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+})
+
+// UPDATE: update job listing
+router.put('/:id', async (req, res) => {
+    const { title, description, company, education, experience } = req.body
+    const newJobListing = { title, description, company, education, experience }
+
+    try {
+        const listing = await ListingModel.findByIdAndUpdate(req.params.id, newJobListing, { returnDocument: 'after' }).populate()
+        if (listing) {
+            res.send(listing)
+        } else {
+            res.status(404).send({ error: 'Job listing not found!' })
+        }
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+})
+
+// DELETE: delete job listing
+router.delete('/:id', async (req, res) => {
+    try{
+        const listing = await ListingModel.findByIdAndDelete(req.params.id)
+        if (listing) {
+            // res.send(204).send({ message: 'Job listing deleted successfully!'})
+            res.sendStatus(204)
+        // } else {
+            // res.sendStatus(404).send({ error: 'Job listing not found!' })
+        } else {
+            res.sendStatus(404).send({ error: 'Job listing not found!' }) // not working for some reason - check later
+        }
+    }
+    catch (err) {
+        res.sendStatus(500).send({ error: err.message})
+    }
+})
+
+export default router
